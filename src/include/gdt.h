7de1bb8d6dc36b30af909ec53ba9752e8b11b0f8
Pablo Antonio <pabloa@gmail.com>
2011-04-28 08:34:10 -0300

    Reemplazar macros, estructuras y definiciones de tipos en gdt.h
    La idea es reutilizar el codigo que habiamos usado en Zafio.

--- src/include/gdt.h
+++ src/include/gdt.h
@@ -3,67 +3,83 @@
 
 #include <tipos.h>
 
-typedef struct str_gdt_descriptor {
-	unsigned short gdt_length;
-	unsigned int gdt_addr;
-} __attribute__((__packed__)) gdt_descriptor;
+/* GDT
+ * ===
+ * 
+ * Convenciones:
+ * - Todas las macros relacionadas con la GDT comienzan con el
+ *   prefijo ``GDT_``.
+ * - Aquellas relacionadas con descriptores en la GDT, llevan el prefijo
+ *   ``GDT_DESC_``. El resto del nombre de la macro lleva el nombre dado en
+ *   los manuales de Intel, con la salvedad de que los espacios se reemplazan
+ *   por ``_``, el caracter ``/`` se omite y todas las letras van en mayuscula.
+ * - Los flags, por comodidad, tienen solo el prefijo ``GDT_F``.
+ *   Los flags para tipos de segmentos de aplicaciones siguen con ``_DATA_X``
+ *   o ``_CODE_X`` segun sean de datos o de codigo, donde X representa el
+ *   nombre que se le da en los manuales a cada bit que compone el tipo.
+ *   Los flags para tipos de sistema siguen con las siglas correspondientes al
+ *   tipo que representan segun el nombre que se le da en los manuales de
+ *   Intel.
+ */
+#define GDT_DESC_BASE(dir) ( ((uint64_t)(dir) & __LOW16_BITS__) << 16 | \
+	((uint64_t)(dir) & __16_23_BITS__) << (32 - 16) | \
+	((uint64_t)(dir) & __24_31_BITS__) << 32 )
 
+#define GDT_DESC_LIMIT(dir) ( ((uint64_t)(dir) & __LOW16_BITS__) | \
+	((uint64_t)(dir) & __16_19_BITS__) << 32 )
 
-typedef union str_gdt_entry {
-	struct str_gdt_entry_fld {
-		unsigned short limit_0;
-		unsigned short base_01;
-		unsigned char base_2;
-		unsigned char type:4;
-		unsigned char s:1;
-		unsigned char dpl:2;
-		unsigned char p:1;
-		unsigned char limit_1:4;
-		unsigned char avl:1;
-		unsigned char l:1;
-		unsigned char db:1;
-		unsigned char g:1;
-		unsigned char base_3;
-	} fld;
-	struct str_gdt_entry_vl {
-		unsigned int vl0, vl1;
-	} vl;
-} __attribute__((__packed__, aligned (8))) gdt_entry;
+#define GDT_DESC_G (((uint64_t) 1) << (32 + 23))
+#define GDT_DESC_DB (((uint64_t) 1) << (32 + 22))
+#define GDT_DESC_L (((uint64_t) 1) << (32 + 21))
+#define GDT_DESC_P (((uint64_t) 1) << (32 + 15))
+#define GDT_DESC_DPL(level) (((uint64_t) level) << (32 + 13))
+#define GDT_DESC_S (((uint64_t) 1) << (32 + 12))
+#define GDT_DESC_TYPE(type) ((uint64_t)(type) << (32 + 8))
 
-/** Tabla GDT **/
-extern gdt_entry gdt[];
-#define GDT_COUNT 256
+// Flags para tipos de aplicaciones
+#define GDT_F_DATA_A    0x1 // accessed
+#define GDT_F_DATA_W    0x2 // write-enable
+#define GDT_F_DATA_E    0x4 // expansion-direction
+#define GDT_F_CODE_A    0x9 // accessed
+#define GDT_F_CODE_R    0xA // read enable
+#define GDT_F_CODE_C    0xC // conforming
+
+// Flags para tipos de sistema
+#define GDT_F_16BTA     0x1 // 16-bit TSS (Available)
+#define GDT_F_LDT       0x2 // LDT
+#define GDT_F_16BTB     0x3 // 16-bit TSS (Busy)
+#define GDT_F_16BCG     0x4 // 16-bite Call Gate
+#define GDT_F_TG        0x5 // Task Gate
+#define GDT_F_16BIG     0x6 // 16-bit Interrupt Gate
+#define GDT_F_16BTG     0x7 // 16-bit Trap Gate
+#define GDT_F_32BTA     0x9 // 32-bit TSS (Available)
+#define GDT_F_32BTB     0xB // 32-bit TSS (Busy)
+#define GDT_F_32BCG     0xC // 32-bit Call Gate
+#define GDT_F_32BIG     0xE // 32-bit Interrupt Gate
+#define GDT_F_32BTG     0xF // 32-bit Trap Gate
+
+#define GDT_NULL ((uint64_t) 0)
+
+#define GDT_SEGSEL(rpl, index) \
+    (((rpl) & __LOW2_BITS__) | ((index) << 3))
+
+typedef struct { 
+    uint16_t size __attribute__((packed));
+    void *addr __attribute__((packed));
+} gdtr_t; 
+
+extern gdtr_t gdtr;
+
+extern uint64_t gdt[];
+
+#define GDT_INDEX_NULL 0x0
+#define GDT_INDEX_KERNEL_CS 0x1
+#define GDT_INDEX_KERNEL_DS 0x2
+#define GDT_INDEX_USER_CS 0x3
+#define GDT_INDEX_USER_DS 0x4
+#define GDT_INDEX_TSS 0x5
 
 void gdt_init(void);
-
-/** Constantes de los atributos **/
-
-/* Masks */
-#define GDT_ATTR_A    0x001
-#define GDT_ATTR_W    0x002
-#define GDT_ATTR_ED   0x004
-#define GDT_ATTR_R    0x002
-#define GDT_ATTR_C    0x004
-#define GDT_ATTR_E    0x008
-#define GDT_ATTR_S    0x010
-#define GDT_ATTR_DPL  0x060
-#define GDT_ATTR_P    0x080
-#define GDT_ATTR_AVL  0x100
-#define GDT_ATTR_DB   0x400
-#define GDT_ATTR_G    0x800
-
-/* DPL Field */
-#define GDT_ATTR_DPL0 0x000
-#define GDT_ATTR_DPL1 0x020
-#define GDT_ATTR_DPL2 0x040
-#define GDT_ATTR_DPL3 0x060
-
-/* S Field */
-#define GDT_ATTR_S_ON  0x000
-#define GDT_ATTR_S_OFF 0x010
-
-/* TYPE Field */
-#define GDT_ATTR_TYPE_TSS 0x009
 
 
 #endif
--- src/include/tipos.h
+++ src/include/tipos.h
@@ -19,4 +19,36 @@ typedef sint_32 pid;
 #define TRUE 1
 #define NULL ((void*)0)
 
+
+/*
+ * Mascaras
+ */
+
+#define __LOW2_BITS__ 0x00000003
+#define __LOW16_BITS__ 0x0000FFFF
+#define __LOW12_BITS__ 0x00000FFF
+#define __HIGH16_BITS__ 0xFFFF0000
+#define __16_23_BITS__ 0x00FF0000
+#define __24_31_BITS__ 0xFF000000
+#define __16_19_BITS__ 0x000F0000
+#define __12_31_BITS__ 0xFFFFF000
+
+#define __22_31_BITS__ 0xFFC00000
+#define __12_21_BITS__ 0x003FF000
+
+
+/* 
+ * Aliases para compatibilidad con codigo de Zafio
+ */
+
+typedef uint_8      uint8_t;
+typedef uint_16     uint16_t;
+typedef uint_32     uint32_t;
+typedef uint_64     uint64_t;
+
+typedef sint_8      int8_t; 
+typedef sint_16     int16_t; 
+typedef sint_32     int32_t; 
+typedef sint_64     int64_t; 
+
 #endif
