66d7b31a32937a35505a29831e1cba3651b941c9
Pablo Antonio <pabloa@gmail.com>
2011-05-17 20:08:21 -0300

    Implementar sched.c de manera naive

--- src/kernel/sched.c
+++ src/kernel/sched.c
@@ -1,31 +1,115 @@
 #include <idt.h>
 #include <sched.h>
 #include <loader.h>
+#include <utils.h>
+
+#define DEFAULT_QUANTUM 20
 
 
-typedef struct str_sched_task {
-	uint_32 state;
-	uint_32 quantum;
-	// ...etc... (se puede modificar todo este struct)
-} sched_task;
+typedef struct sched_task sched_task;
+struct sched_task {
+    bool blocked;
+    uint32_t quantum;
+    uint32_t rem_quantum;
+
+    sched_task *next;
+    sched_task *prev;
+};
+
+static sched_task *next_executable_task(sched_task *task);
+static sched_task *current_task();
+static int get_pid(sched_task *task);
+static void restart_quantum(sched_task *task);
+
+
 sched_task tasks[MAX_PID];
 
+// Lista de tareas del scheduler
+sched_task *task_list;
+// Lista de tareas libres
+sched_task *free_tasks;
 
 void sched_init(void) {
+    memset(tasks, 0, sizeof(tasks));
+
+    free_tasks = NULL;
+    int i;
+    for (i = 0; i < MAX_PID; i++) {
+        APPEND(&free_tasks, &tasks[i]);
+    }
 }
 
+
 void sched_load(pid pd) {
+    sched_task *task;
+    POP(&free_tasks, &task);
+
+    // Llenar task
+    task->blocked = FALSE;
+    task->quantum = DEFAULT_QUANTUM;
+    task->rem_quantum = task->quantum;
+
+    APPEND(&task_list, task);
 }
 
 void sched_unblock(pid pd) {
+    sched_task *task = current_task();
+    task->blocked = FALSE;
 }
 
 int sched_exit() {
-	return 0;
+    sched_task *task = current_task();
+
+    UNLINK_NODE(&task_list, task);
+    APPEND(&free_tasks, task);
+
+    return get_pid(next_executable_task(task));
 }
+
 int sched_block() {
-	return 0;
+    sched_task *task = current_task();
+    task->blocked = TRUE;
+	return get_pid(task->next);
 }
+
 int sched_tick() {
-	return 0;
+    sched_task *current = current_task();
+    current->rem_quantum--;
+
+    if (!current->rem_quantum) {
+        restart_quantum(current);
+
+        sched_task *next_task = next_executable_task(current);
+        if (next_task)
+            return get_pid(next_task);
+    }
+
+    // Si la tarea actual aun tiene quantum o no encontramos otra tarea a
+    // ejecutar, seguimos con la misma
+	return get_pid(current);
+}
+
+static sched_task *next_executable_task(sched_task *task) {
+    sched_task *candidate = task->next;
+    while (candidate != task) {
+        if (!(candidate->blocked))
+            return candidate;
+
+        candidate = candidate->next;
+    }
+    // Nunca deberiamos llegar aqui si hay siempre una tarea idle
+    return NULL;
+}
+
+
+static void restart_quantum(sched_task *task) {
+    task->rem_quantum = task->quantum;
+}
+
+static int get_pid(sched_task *task) {
+    return (task - task_list);
+}
+
+static sched_task *current_task() {
+    return task_list;
 }
