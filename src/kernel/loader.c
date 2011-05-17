d8e60b15aead024aae454a38e8d8f98a5b471aff
Pablo Antonio <pabloa@gmail.com>
2011-05-17 20:05:26 -0300

    Agregar struct de TSS al loader

--- src/kernel/loader.c
+++ src/kernel/loader.c
@@ -1,5 +1,55 @@
 #include <loader.h>
 
+/*
+ * TSS
+ */
+typedef struct {
+    uint16_t prev;  // Previous Task Link
+    uint16_t :16;
+    uint32_t esp0;  // Stack pointer (nivel 0)
+    uint16_t ss0;   // Stack segment (nivel 0)
+    uint16_t :16;
+    uint32_t esp1;  // Stack pointer (nivel 1)
+    uint16_t ss1;   // Stack segment (nivel 1)
+    uint16_t :16;
+    uint32_t esp2;  // Stack pointer (nivel 2)
+    uint16_t ss2;   // Stack segment (nivel 2)
+    uint16_t :16;
+    uint32_t cr3;   // Page directory base register
+    uint32_t eip;
+    uint32_t eflags;
+
+    // Registros de proposito general
+    uint32_t eax;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t ebx;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t esi;
+    uint32_t edi;
+
+    // Registros de segmento
+    uint16_t es;
+    uint16_t :16;
+    uint16_t cs;
+    uint16_t :16;
+    uint16_t ss;
+    uint16_t :16;
+    uint16_t ds;
+    uint16_t :16;
+    uint16_t fs;
+    uint16_t :16;
+    uint16_t gs;
+    uint16_t :16;
+    uint16_t ldt;
+    uint16_t :16;
+
+    uint16_t t:1;   // Debug trap flag
+    uint16_t :15;
+    uint16_t io;    // I/O map base address
+} tss_t;
+
 task task_table[MAX_PID];
 
 /* pid "actual" */
