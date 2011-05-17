5a2fc74f9eb518322d6a61f2fc7f84c95cd11b57
Pablo Antonio <pabloa@gmail.com>
2011-05-17 20:04:22 -0300

    Agregar macros para implementar listas enlazadas

--- src/include/utils.h
+++ src/include/utils.h
@@ -9,5 +9,43 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void * ptr, int value, size_t num);
 
 void custom_kpanic_msg(char* custom_msg);
-    
+
+#define LINK_NODES(fst, sec) \
+    (fst)->next = (sec); \
+    (sec)->prev = (fst);
+
+// UNLINK_NODE(<node type> **list, <node type> *node);
+#define UNLINK_NODE(list, node) \
+    if (*(list) != NULL) { \
+        if ((node)->next == (node)) { \
+            (node)->next = NULL; \
+            (node)->prev = NULL; \
+            *(list) = NULL; \
+        } \
+        else { \
+            if (*(list) == (node)) \
+                *(list) = (node)->next; \
+            (node)->next->prev = (node)->prev; \
+            (node)->prev->next = (node)->next; \
+        } \
+    }
+
+// APPEND(<node type> **list, <node type> *node);
+#define APPEND(list, node) \
+    if (*(list) == NULL) { \
+        (*(list)) = (node); \
+        (node)->next = (node); \
+        (node)->prev = (node); \
+    } \
+    else { \
+        LINK_NODES((*(list))->next, (node)); \
+        LINK_NODES((node), (*(list))); \
+    }
+
+// POP(<node type> **list, <node type> **node);
+#define POP(list, node) \
+    *(node) = *(list); \
+    UNLINK_NODE((list), *(node));
+
+
 #endif
