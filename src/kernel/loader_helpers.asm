1d035dd68f27a7a4aad0901a08ddb408eb50ed3c
Pablo Antonio <pabloa@gmail.com>
2011-05-18 09:20:55 -0300

    Agregar loader_helpers.asm

---
+++ src/kernel/loader_helpers.asm
@@ -0,0 +1,22 @@
+%include "./include/nasm_macros.inc"
+
+global loader_switch_stack_pointers
+
+%define old_stack_top_ptr [ebp + 8]
+%define new_stack_top_ptr [ebp + 12]
+loader_switch_stack_pointers:
+    push ebp
+    mov ebp, esp
+    
+    mov eax, old_stack_top_ptr
+    mov ecx, new_stack_top_ptr
+    
+    ; Guardamos el esp de la tarea vieja
+    mov [eax], esp
+
+    ; Cargamos el esp de la tarea a la que vamos
+    mov esp, [ecx]
+
+    pop ebp
+
+    ret
