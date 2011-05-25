8714f59fa4a64c6afcd3b6542ae6f0b66af2bdad
Pablo Antonio <pabloa@gmail.com>
2011-05-25 04:21:13 -0300

    Implementar semaforos

--- src/kernel/sem.c
+++ src/kernel/sem.c
@@ -1,8 +1,33 @@
 #include <sem.h>
+#include <loader.h>
+#include <i386.h>
 
 void sem_wait(sem_t* s) {
+    sti();
+
+    s->vl--;
+    if (s->vl < 0)
+        loader_enqueue(&s->q);
+
+    cli();
 }
+
 void sem_signaln(sem_t* s) {
+    sti();
+
+    if (s->vl < 0)
+        loader_unqueue(&s->q);
+    s->vl++;
+
+    cli();
 }
+
 void sem_broadcast(sem_t* s) {
+    sti();
+
+    while (s->q != -1)
+        loader_unqueue(&s->q);
+    s->vl = 0;
+
+    cli();
 }
