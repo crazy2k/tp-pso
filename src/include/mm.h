e13a5e9040b64b6acf8a4cab22c59ca738d95b87
Pablo Herrero <pablodherrero@gmail.com>
2011-05-25 03:25:13 -0300

    Completed loader_exit

--- src/include/mm.h
+++ src/include/mm.h
@@ -101,6 +101,7 @@ typedef struct str_mm_page {
 #define LAST_POSIBLE_PAGE 0x100000
 #define KERNEL_MEMORY_START ((void*)0x0)
 #define KERNEL_MEMORY_LIMIT ((void*)0x400000)
+#define USER_MEMORY_START ((void*)0x400000)
 #define KERNEL_STACK (0xA0000 - PAGE_SIZE)
 
 #define TEST_WORD 0xF0F0F0F0
