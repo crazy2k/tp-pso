51a7a85d613cbc9c9702acdd0dfeb496e2a8de3b
Pablo Herrero <pablodherrero@gmail.com>
2011-06-19 12:30:49 -0300

    Fix read_from_blockdev

--- src/kernel/utils.c
+++ src/kernel/utils.c
@@ -91,16 +91,21 @@ int read_from_blockdev(blockdev *bdev, uint64_t offset, void *buf, uint32_t size
 
     uint32_t remainder = offset % bdev->size,
              pos = offset / bdev->size,
-             sectors_size = align_to_next(size + remainder, bdev->size),
-             last_remainder = sectors_size - size - remainder;
+             last_remainder = bdev->size - ((size + remainder) % bdev->size),
+             sectors_size = size + remainder + last_remainder,
+             middle_sectors_size = sectors_size - remainder - last_remainder;
 
     bdev->read(bdev, pos, read_buf, bdev->size);
     memcpy(buf, read_buf + remainder, bdev->size - remainder);
 
-    bdev->read(bdev, pos + 1, buf + bdev->size - remainder, sectors_size - bdev->size * 2);
+    if (size - (bdev->size - remainder) >= bdev->size)
+        bdev->read(bdev, pos + 1, buf + bdev->size - remainder, middle_sectors_size);
+    
 
-    bdev->read(bdev, (sectors_size/bdev->size) - 1, read_buf, bdev->size);
-    memcpy(buf + (size - (bdev-> size - last_remainder)), read_buf, last_remainder);
+    if (last_remainder > 0) {
+        bdev->read(bdev, (sectors_size/bdev->size) - 1, read_buf, bdev->size);
+        memcpy(buf + (size - (bdev->size - last_remainder)), read_buf, (bdev->size - last_remainder));
+    }
     
     return size;
 }
