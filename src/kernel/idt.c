b20507eac8b903a7619018332b011507c40bd46b
Pablo Herrero <pablodherrero@gmail.com>
2011-07-06 11:08:36 -0300

    Set return value for syscall pipe

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -176,7 +176,7 @@ static void syscall_caller(uint32_t index, uint32_t error_code, task_state_t
             sys_run((char *)st->ebx);
             break;
         case SYSCALLS_NUM_PIPE:
-            sys_pipe((int *)st->ebx);
+            st->eax = sys_pipe((int *)st->ebx);
             break;
 
     }
