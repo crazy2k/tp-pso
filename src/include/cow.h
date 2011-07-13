4f758516fe695518bf17cc38c4523f09d2f16d5a
Pablo Herrero <pablodherrero@gmail.com>
2011-07-13 02:01:12 -0300

    Add cow implementation stub

--- src/include/cow.h
+++ src/include/cow.h
@@ -3,4 +3,8 @@
 
 #include <tipos.h>
 
+
+uint32_t cow_make_cow_pte(uint32_t pte);
+uint32_t cow_void_cow_pte(uint32_t pte);
+
 #endif
--- src/kernel/cow.c
+++ src/kernel/cow.c
@@ -1,5 +1,10 @@
 #include <cow.h>
+#include <mm.h>
+#include <utils.h>
 
+
+#define HASH_TABLE_LEGTH 256
+#define FREE_NODES_LEGTH 256
 
 typedef struct cow_t cow_t;
 struct cow_t {
@@ -9,3 +14,30 @@ struct cow_t {
     cow_t *next;
     cow_t *prev;
 };
+
+
+static cow_t cow_hash_table[HASH_TABLE_LEGTH] = { {0} };
+static cow_t cow_nodes[FREE_NODES_LEGTH] = { {0} };
+
+static cow_t* free_cow_nodes = 0;
+
+void cow_init(void) {
+    CREATE_FREE_OBJS_LIST(free_cow_nodes, cow_nodes, FREE_NODES_LEGTH);
+}
+
+
+uint32_t cow_void_cow_pte(uint32_t pte) {
+
+    return NULL;
+}
+
+uint32_t cow_make_cow_pte(uint32_t pte) {
+    if (PTE_IS_COW_PAGE(pte)) {
+        // TODO: Search cow node and increment refcount
+        
+    } else {
+        // TODO: Create cow node
+    
+    }
+    return NULL;
+}
