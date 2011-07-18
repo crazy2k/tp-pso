6ff76aea1929bc999f5d532212c15cf9fc8bb495
Pablo Antonio <pabloa@gmail.com>
2011-07-18 08:34:05 -0300

    Quitar XXX innecesario

--- src/kernel/ext2.c
+++ src/kernel/ext2.c
@@ -395,7 +395,6 @@ static int get_data(ext2 *part_info, ext2_inode *inode, void *buf) {
 
 /* Por ahora solo soporta archivos de 12 bloques (si los bloques son de 1024,
  * son 12KB).
- * XXX: El soporte para bloques indirectos no esta probado.
  */
 static int get_data_for_file(ext2 *part_info, ext2_inode *inode, uint32_t first_bno, void *buf, uint32_t buf_size) {
     // Chequeamos si el archivo es mas grande que el buffer que tenemos
