c1b1fa484a55621c04e45c21fba7dcba42743f45
Pablo Antonio <pabloa@gmail.com>
2011-07-22 20:52:28 -0300

    Agregar decrypt.c
    Por alguna razón, no desencripta bien la parte final del archivo.

---
+++ src/decrypt.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
+
+char *key_buf = "miabuelasellamaanita";
+char buf[BUF_SIZE];
+
+int main(int argc, char *argv[]) {
+    int fd = open(argv[1], O_RDONLY);
+    int n;
+    while (n = read(fd, buf, BUF_SIZE)) {
+        int i;
+        for (i = 0; i < n; i++) {
+            buf[i] = buf[i] ^ key_buf[i % strlen(key_buf)];
+            write(1, buf + i, 1);
+        }
+    }
+}
